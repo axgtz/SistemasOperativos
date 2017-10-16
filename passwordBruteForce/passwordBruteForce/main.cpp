a46a182a5c882eb2307d51f7671de10c77c87120
axgtz <axguti@gmail.com>
2017-10-15 22:47:50 -0500

    Idk

--- passwordBruteForce/passwordBruteForce/main.cpp
+++ passwordBruteForce/passwordBruteForce/main.cpp
@@ -32,6 +32,7 @@ void *productor(void *ptr) /* produce datos */{
 		
 		bufer = i; /* coloca elemento en el búfer */
 		cout<<"coloca"<<endl;
+		
 		pthread_cond_signal(&condc); /* despierta al consumidor */
 		pthread_mutex_unlock(&el_mutex); /* libera el acceso al búfer */
 	}
@@ -44,8 +45,10 @@ void *consumidor(void *ptr) /* consume datos */{
 	for (i=1; i<=MAX; i++) {
 		pthread_mutex_lock(&el_mutex); /* obtiene acceso exclusivo al búfer */
 		while (bufer==0) pthread_cond_wait(&condc, &el_mutex);
+		
 		bufer = 0; /* saca elemento del búfer */
 		cout<<"salir"<<endl;
+		
 		pthread_cond_signal(&condp); /* despierta al productor */
 		pthread_mutex_unlock(&el_mutex); /* libera el acceso al búfer */
 	}
@@ -55,13 +58,18 @@ void *consumidor(void *ptr) /* consume datos */{
 int main(int argc, char **argv){
 	pthread_t pro, con;
 	pthread_mutex_init(&el_mutex, 0);
+	
 	pthread_cond_init(&condc, 0);
 	pthread_cond_init(&condp, 0);
+	
 	pthread_create(&con, 0, consumidor, 0);
 	pthread_create(&pro, 0, productor, 0);
+	
 	pthread_join(pro, 0);
 	pthread_join(con, 0);
+	
 	pthread_cond_destroy(&condc);
 	pthread_cond_destroy(&condp);
+	
 	pthread_mutex_destroy(&el_mutex);
 }
