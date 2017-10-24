2a81a7ccf85016649b2a7f5c74857b687820fc75
axgtz <axguti@gmail.com>
2017-10-24 18:12:36 -0500

    tstes

--- passwordBruteForce/passwordBruteForce/generatorTest.cpp
+++ passwordBruteForce/passwordBruteForce/generatorTest.cpp
@@ -10,48 +10,42 @@
 
 using namespace std;
 // Create some alphabet tables
+string hashABuscar;
 
-char AlphabetUpper[69] = {'0','1','2','3','4','5','6','7','8','9','@','#','$','%','^','&','*','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+static unsigned int length;//Longitud de palabra a generar
+static unsigned int stringLength;
+unsigned int indiceAlfabeto;
+
+char Alphabet[69] = {'0','1','2','3','4','5','6','7','8','9','@','#','$','%','^','&','*','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
 // Recursive function, keeps clocking characters
 // until length is reached
 
-bool Generate(unsigned int length, string s, string hash){
-    if(length == 0){ // when length has been reached
-        if(md5(s) == hash){
-            cout << "Se encontro: " << s << "\n";
-            return false;
-        }
-        //cout << s << "\n"; // print it out
-        return true;
-    }
-    // iterate through alphabet
-    for(unsigned int i = 0; i < 69; i++){
-        // Create new string with next character
-        // Call generate again until string has reached it's length
-        string appended = s + AlphabetUpper[i];
-        if(!Generate(length-1, appended, hash)){
-            return false;
-        }
-    }
-    return true;
-}
 
-bool static Crack(string hash){
-    bool seguir = true;
-    while(seguir){
-        // Keep growing till I get it right
-        static unsigned int stringlength = 1;
-        seguir= Generate(stringlength, "", hash);
-        if(seguir){
-            cout << "No se encontro la palabra en ninguna permutacion con longitud: " << stringlength<< "\n";
-        }
-        stringlength++;
+void Generate(string s){
+    if(length == 0){ // when length has been reached
+        cout << s << "\n"; // print it out
+        return;
     }
-    return true;
+    for(unsigned int i = 0; i < 69; i++){
+        string appended = s + Alphabet[i];
+        
+        if(length>0){
+            length--;
+        }
+        Generate(appended);
+    }
+}
+void Crack(){
+    length = 1;
+    while (1) {
+        Generate("");
+        length++;
+    }
 }
 
 int main(){
-    Crack("32390fa731a71f4cdcf6b76a05334545");
+    hashABuscar = "32390fa731a71f4cdcf6b76a05334545";
+    Crack();
     return 0;
 }
--- passwordBruteForce/passwordBruteForce/main.cpp
+++ passwordBruteForce/passwordBruteForce/main.cpp
@@ -9,7 +9,7 @@
 #include <pthread.h>
 #include "md5.cpp"
 
-#define ELEMS 1000000 //Limite de palabras a generar
+//#define ELEMS 1000000 //Limite de palabras a generar
 #define NUM_THREADS 2
 
 using namespace std;
@@ -18,48 +18,52 @@ pthread_mutex_t el_mutex; //Define si el hilo puede entrar o no a la region crit
 pthread_cond_t condc,condp;
 string bufer; /* búfer utilizado entre productor y consumidor */
 string hashABuscar;
-char AlphabetUpper[69] = {'0','1','2','3','4','5','6','7','8','9','@','#','$','%','^','&','*','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+unsigned int length;//Longitud de palabra a generar
+unsigned int stringlLength;
+unsigned int indiceAlfabeto;
+//bool inicio;
+
+char Alphabet[69] = {'0','1','2','3','4','5','6','7','8','9','@','#','$','%','^','&','*','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 //--------------PassGen------------
 //"0123456789""!@#$%^&*""ABCDEFGHIJKLMNOPQRSTUVWXYZ""abcdefghijklmnopqrstuvwxyz"
+/*bool generator(bool inicio){
+	while(inicio){
+		
+	}
+	
+	return false;
+}*/
 
-bool Generate(unsigned int length, string s){
+void Generate(string s){
 	if(length == 0){ // when length has been reached
-		if(md5(s) == hashABuscar){
-			cout << "Se encontro: " << s << "\n";
-			return false;
-		}
-		//cout << s << "\n"; // print it out
-		return true;
+		cout << s << "\n"; // print it out
+		return;
 	}
-	// iterate through alphabet
 	for(unsigned int i = 0; i < 69; i++){
-		// Create new string with next character
-		// Call generate again until string has reached it's length
-		string appended = s + AlphabetUpper[i];
-		if(!Generate(length-1, appended)){
-			return false;
-		}
+		string appended = s + Alphabet[i];
+		length--;
+		Generate(appended);
 	}
-	return true;
 }
-
-bool Crack(){
-	bool seguir = true;
-	while(seguir){
-		static unsigned int stringlength = 1;
-		seguir= Generate(stringlength, "");
-		if(seguir){
-			cout << "No se encontro la palabra en ninguna permutacion con longitud: " << stringlength<< "\n";
-		}
-		stringlength++;
+void Crack(){
+	while (1) {
+		stringlLength = 1;
+		Generate("");
+		length++;
 	}
-	return true;
 }
 
 void *productor(void *ptr) /* produce datos */{
 	pthread_mutex_lock(&el_mutex); /* obtiene acceso exclusivo al búfer */
 	while (bufer[0] != NULL) pthread_cond_wait(&condc, &el_mutex);
-
+	//inicio = true;
+	//generator(inicio);
+	if(indiceAlfabeto>=69){
+		indiceAlfabeto=0;
+		length++;
+	}
+	
 	//bufer[0] = ' ';
 	pthread_cond_signal(&condc); /* despierta al consumidor */
 	pthread_mutex_unlock(&el_mutex); /* libera el acceso al búfer */
@@ -85,8 +89,7 @@ void *consumidor(void *ptr) /* consume datos */{
 int main(){
 	hashABuscar = "32390fa731a71f4cdcf6b76a05334545";
 	Crack();
-
-	pthread_t pro, con;
+	/*pthread_t pro, con;
 	pthread_mutex_init(&el_mutex, 0);
 	pthread_cond_init(&condc, 0);
 	pthread_cond_init(&condp, 0);
@@ -97,6 +100,6 @@ int main(){
 	 
 	pthread_cond_destroy(&condc);
 	pthread_cond_destroy(&condp);
-	pthread_mutex_destroy(&el_mutex);
+	pthread_mutex_destroy(&el_mutex);*/
 	return 0;
 }
